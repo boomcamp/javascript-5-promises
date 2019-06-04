837872ed5ee505ff878d0e572c4d62e998a653c7
Jules <jules.ballaran@boom.camp>
2019-06-04 13:31:23 +0800

    done 3

--- practice.js
+++ practice.js
@@ -55,6 +55,14 @@ function last(){
 */
 
 //Code Here
+function toLarge(n){
+  return new Promise(function (resolve, reject) {
+    if(n<10)
+      resolve(n)
+    else
+      reject(n)
+  })
+}
 
 
 ////////// PROBLEM 4 //////////
