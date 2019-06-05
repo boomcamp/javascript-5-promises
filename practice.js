aa534b7cf513762ae3d93b090369f4bc940ffe9d
Alan John Alilano <alan.alilano@boom.camp>
2019-06-05 10:09:31 +0800

    Finished Problem 1

--- practice.js
+++ practice.js
@@ -27,7 +27,11 @@ function promisedNames() {
  */
 
 // Code Here
-
+function first(){
+  return new Promise((resolve, reject) => {
+    resolve('Aodhan');
+  });
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -95,5 +99,4 @@ var modifiedResolve;
 /////////////////////////////////
 
 //Code Here
-
 
